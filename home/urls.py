d3b1b3eb109d2ab0f1f5ee4770a518565fe1477e
GraceObaEllyone <62464629+GraceObaEllyone@users.noreply.github.com>
2022-04-21 10:10:50 +0100

    Course Web

---
+++ home/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.index, name='home'),
+    # path('contactform', views.contactform, name='contactform'),
+    # path('thankyou_page/', views.thankyou_page, name='thankyou_page'),
+]
