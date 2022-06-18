4e14a99ae9c6513d78d2d4719327ac624132f63f
GraceObaEllyone <62464629+GraceObaEllyone@users.noreply.github.com>
2022-06-18 12:00:26 +0100

    Update settings.py

--- CourseFinder/settings.py
+++ CourseFinder/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 DEBUG = True
 
 
-ALLOWED_HOSTS = ['elyoneducomm.heroku.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['elyoneducomm.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
