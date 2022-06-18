93b159244647d0b8d546ea8d9346ed98d998119c
Rahul Mohite <rahulmohite.sid@gmail.com>
2022-06-18 08:08:21 +0100

    final changes

---
+++ static/js/plugins 2.js
@@ -0,0 +1,31 @@
+
+//  Bootstrap Bundle Min Js
+document.writeln('<script src="js/bootstrap.bundle.min.js" type="text/javascript"></script>');
+
+// Meanmenu Min Js
+document.writeln('<script src="js/meanmenu.min.js" type="text/javascript"></script>');
+
+// Ajaxchimp Min Js
+document.writeln('<script src="js/ajaxchimp.min.js" type="text/javascript"></script>');
+
+// Form Validator Min Js
+document.writeln('<script src="js/form-validator.min.js" type="text/javascript"></script>');
+
+// Contact Form  Js
+document.writeln('<script src="js/contact-form-script.js" type="text/javascript"></script>');
+
+// Owl Carousel  Js
+document.writeln('<script src="js/owl.carousel.min.js" type="text/javascript"></script>');
+
+// Magnific Popup  Js
+document.writeln('<script src="js/magnific-popup.min.js" type="text/javascript"></script>');
+
+// AOS  Js
+document.writeln('<script src="js/aos.js" type="text/javascript"></script>');
+
+// Odometer JS
+document.writeln('<script src="js/odometer.min.js" type="text/javascript"></script>');
+document.writeln('<script src="js/appear.min.js" type="text/javascript"></script>');
+
+// TweenMax min JS
+document.writeln('<script src="js/tweenMax.min.js" type="text/javascript"></script>');
