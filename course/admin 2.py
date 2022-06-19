7230a56cda2c85c943db9ae9fa06a6cfffe8df31
GraceObaEllyone <62464629+GraceObaEllyone@users.noreply.github.com>
2022-06-19 18:07:10 +0100

    delete extra files

--- course/admin 2.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from course.models import Course, CourseReview, Category
-
-admin.site.register(Course)
-admin.site.register(CourseReview)
-admin.site.register(Category)
