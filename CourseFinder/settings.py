e56fd17a28221f782fc84f6d647738ea67505508
Rahul Mohite <rahulmohite.sid@gmail.com>
2022-06-19 19:36:00 +0100

    gh

--- CourseFinder/settings.py
+++ CourseFinder/settings.py
@@ -28,7 +28,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 DEBUG = True
 
 
-ALLOWED_HOSTS = ['elyoneducomm.herokuapp.com', 'localhost', '127.0.0.1']
+# ALLOWED_HOSTS = ['elyoneducomm.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = []
 
 
 # Application definition
