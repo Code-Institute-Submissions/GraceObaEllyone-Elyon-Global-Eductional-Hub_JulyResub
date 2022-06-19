13dd6144b3ff6d856203d9ec9e1348df98cac721
Rahul Mohite <rahulmohite.sid@gmail.com>
2022-06-19 19:32:27 +0100

    submit

--- profiles/admin 2.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from profiles.models import UserProfile, UserGroup
-
-admin.site.register(UserProfile)
-admin.site.register(UserGroup)
--- profiles/urls 2.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from profiles import views
-
-urlpatterns = [
-    path('update/', views.update_details, name='update_profile'),
-]
